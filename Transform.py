99f5541bf62ed23cc40c464752e9de17c08ef93e
palinax <palinatx@gmail.com>
2024-05-15 15:59:10 +0200

    Merge, add new columns, extract category, derive country

--- Transform.py
+++ Transform.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from datetime import datetime
+import pycountry
+from Utility import EU_COUNTRY_CODES
 
 #  Convert a column in DataFrame to datetime format, coercing errors
 
@@ -19,3 +21,35 @@ def calculate_age(df, date_column):
 def filter_and_select(df, condition, columns):
     filtered_df = df.query(condition)
     return filtered_df[columns]
+# Merge two DataFrames on a specified key
+
+def merge_dataframes(df1, df2, on_key, how_type='inner'):
+    return pd.merge(df1, df2, on=on_key, how=how_type)
+
+# Ad new columns 
+    
+def add_new_columns(df, iso_codes_column, category_list_column):
+    df['has_a_lead_investor'] = df['lead_investor_uuids'].notna().astype(int)
+    df['first_category'] = df[category_list_column].str.split(',').str[0]
+    df['country'] = df[iso_codes_column].apply(lambda x: pycountry.countries.get(alpha_3=x).name if pycountry.countries.get(alpha_3=x) else None)
+    df['num_exits'] = df['num_exits'].fillna(0)
+    return df
+
+# Extract the first category from a column containing comma-separated values
+
+def extract_first_category(df, column):
+    df['first_category'] = df[column].str.split(',').str[0]
+    return df
+
+# Get the full country name from an ISO country code
+def get_country_name(iso_code):
+    try:
+        return pycountry.countries.get(alpha_3=iso_code).name
+    except:
+        return None
+    
+# Add a column with country names derived from ISO country codes
+
+def add_country_names(df, iso_column):
+    df['country'] = df[iso_column].apply(get_country_name)
+    return df
